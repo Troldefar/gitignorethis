6176137aa6ead8c51cb7fc17ec58f60432fb2489
rasmus <r_emil@live.dk>
2022-09-25 18:32:53 +0200

    regex for routing

--- core/Router.php
+++ core/Router.php
@@ -26,19 +26,21 @@ class Router {
     protected array $params = [];
 
     /**
-     * Regex for checking if valid controller / action
-     * Minor regex to check if controller / action exists
-     * @var string
-    */
-
-    protected string $validityHandler = '/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/';
-
-    /**
      * Setter for routes
+     * Step 1 converts the passed route to a regex thus escaping forward slashes
+     * step 2 converts variables etc {controller}
+     * step 3 makes it able to handle options arguments etc /posts/2379/delete
+     * step 4 Adds start and end delimters including the insensitive flag
+     * step 5 sets the route
      * @return void
     */
 
-    public function setRoute(string $route, array $params): void {
+    public function setRoute(string $route, array $params = []): void {
+        $route = preg_replace('/\//', '\\/', $route);
+        var_dump($route);echo'<br>';
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+        $route = '/^' . $route . '$/i';
         $this->routes[$route] = $params;
     }
 
@@ -47,24 +49,39 @@ class Router {
      * @return route
     */
 
-    public function getRoutes(string $key = '') {
-        if ($key === '') return $this->routes;
-        return $this->routes[$key];
+    public function getRoutes(string $optionalKey = '') {
+        if ($optionalKey === '') return $this->routes;
+        return $this->routes[$optionalKey];
     }
 
     /**
      * Check if requsted controller/method exists
+     * If there's a match set additional params
      * @return boolean
     */
 
     public function match(string $url): bool {
-        if (preg_match($this->validityHandler, $url, $matches)) {
-            $params = [];
-            foreach ($matches as $key => $match) if (is_string($key)) $params[$key] = $match;
-            $this->setParamsIfRouteMatch($params);
-            return true;
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                $params = [];
+                foreach ($matches as $key => $match) if (is_string($key)) $params[$key] = $match;
+                $this->params = $params;
+                return true;
+            }
         }
         return false;
+    }
+
+    /**
+     * Loop matches based on regex 
+     * set params accordingly if present
+     * @return void
+    */
+
+    public function loopMatches(array $matches): void {
+        $params = [];
+        foreach ($matches as $key => $match) if (is_string($key)) $params[$key] = $match;
+        $this->setParams($params);
     }
 
     /**
@@ -72,7 +89,7 @@ class Router {
      * @return void
     */
 
-    public function setParamsIfRouteMatch(array $params): void {
+    public function setParams(array $params): void {
         $this->params = $params;
     }
 
@@ -83,6 +100,20 @@ class Router {
 
     public function getParams(): array {
         return $this->params;
+    }
+
+    /**
+     * Setter for routes
+     * @return router
+    */
+
+    public function setTable(): Router {
+        $this->setRoute('', ['controller' => 'Home', 'action' => 'index']);
+        $this->setRoute('posts', ['controller' => 'Posts', 'action' => 'index']);
+        $this->setRoute('{controller}/{action}');
+        $this->setRoute('admin/{controller}/{action}');
+        $this->setRoute('{controller}/{id:\d+}/{action}');
+        return $this;
     }
 
 }
--- public/index.php
+++ public/index.php
@@ -3,17 +3,22 @@
 /*************************
  * Front
  * Routing module
+ * @package app\core\router
  * 
 *************************/
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once '../Core/Router.php';
 
 $router = new App\Core\Router();
-
-$router->setRoute('', ['controller' => 'Home', 'action' => 'index']);
-$router->setRoute('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->setRoute('posts/new', ['controller' => 'Posts', 'action' => 'create']);
+$router->setTable();
 
 $url = $_SERVER['QUERY_STRING'];
-
 $router->match($url);
+
+echo '<pre>';
+echo htmlspecialchars(print_r($router->getRoutes()), true);
+echo '</pre>';
